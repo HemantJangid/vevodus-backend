b7bb5b42955ff9823e28fca4e80914747178263d
Hemant Jangid <HJangid@chenoainc.com>
2021-09-02 16:37:17 +0530

    changed port address

--- util/index.js
+++ util/index.js
@@ -4,25 +4,23 @@ const port = 3000
 
 const shopDetail = require('./ShopController');
 const DBUtil = require('./connectionEstablish');
-    DBUtil.healthCheck((i)=>{
-        if(i != 'OK') {
-            console.log("closing server. DB Connection Failed");
-            server.close();
-        }
+DBUtil.healthCheck((i) => {
+    if (i != 'OK') {
+        console.log("closing server. DB Connection Failed");
+        server.close();
     }
-);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.get('/api/v1/shop/getdetails', (req, res) =>{
+app.get('/api/v1/shop/getdetails', (req, res) => {
     var userID = req.query['userid'];
-    if(userID == null) {
+    if (userID == null) {
         res.send("Missing user id.");
-    }
-    else {
-        shopDetail.getShopDetails((i) =>{
+    } else {
+        shopDetail.getShopDetails((i) => {
             res.send(i);
         }, userID)
 
@@ -31,5 +29,5 @@ app.get('/api/v1/shop/getdetails', (req, res) =>{
 
 
 var server = app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
