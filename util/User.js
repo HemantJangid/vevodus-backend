7fb0054c61b65149e73572d4da65a1519ac8d272
umesh95 <jangidumesh95@gmail.com>
2021-09-04 17:45:22 +0530

    Add files via upload

--- util/User.js
+++ util/User.js
@@ -1,20 +1,18 @@
 const DBUtil = require('./DBUtil');
 
 
-
-
-
 exports.authenticate = (callback, userDetails) =>{
-	var query = "select a.USER_ID, a.Name, a.EMAILADRESS, a.MOBILE from VD_USER_PROFILE as a inner join USER_PASSWORD as b on a.USER_ID = b.USER_ID and b.PASSWORD = '" + userDetails.password + "' and a.MOBILE='" + userDetails.mobile + "'";
+	var query = "select a.USER_ID, a.Name, a.EMAILADRESS, a.MOBILE, a.QOUTA from VD_USER_PROFILE as a inner join USER_PASSWORD as b on a.USER_ID = b.USER_ID and b.PASSWORD = '" + userDetails.password + "' and a.MOBILE='" + userDetails.mobile + "'";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
 				callback(recordsets['recordset']);
 		}
 		else{
-			callback("Internal Server Error");
+			callback("ERROR");
 			console.log("Internl server error" + err);
 		}
 
 	});
 
 };
+
