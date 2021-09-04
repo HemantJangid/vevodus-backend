1510b01739fdf50499d1c3c73e8c339c84f706c3
umesh95 <jangidumesh95@gmail.com>
2021-09-04 17:06:00 +0530

    Added more APIs

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const multer = require('multer');
-
 const uploads = multer({ dest: "public/files" })
 const fileUpload = require('express-fileupload');
-
 const express = require('express')
-
 const app = express()
 const port = 3000
-var bodyParser     =         require("body-parser");
+var bodyParser  = require("body-parser");
 
 
 const shopDetail = require('./util/ShopController');
-const user = require('./util//User');
-const product = require('./util//Product');
+const user = require('./util/User');
+const product = require('./util/Product');
+const categories = require('./util/Category');
+
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 var AWS = require('aws-sdk');
 AWS.config.update({
@@ -21,6 +25,10 @@ AWS.config.update({
         region: "ap-south-1" //Region
 })
 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 
 const DBUtil = require('./util/connectionEstablish');
@@ -31,15 +39,7 @@ const DBUtil = require('./util/connectionEstablish');
         }
     }
 );
-    app.use(fileUpload());
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.get('/api/v1/shop/getdetails', (req, res) =>{
     var userID = req.query['userid'];
@@ -54,8 +54,23 @@ app.get('/api/v1/shop/getdetails', (req, res) =>{
     }
 });
 
+app.get('/api/v1/getAllShops', (req, res) =>{
+    shopDetail.getAllShops((i) =>{
+            res.send(i);
+    });
+})
+
+app.post('/api/v1/shop/statusChanged', (req, res) => {
+    var shopAttrs = {
+        shopIDs : req.body.shopids,
+        isVerified : req.body.isverified,
+    }
+    shopDetail.changeStatusShop((i) =>{
+            res.send(i);
+    }, shopAttrs);
+});
+
 app.get('/api/v1/product/getProducts' , (req,res) =>{
-    
     var userID = req.query['shopid'];
     if(userID == null) {
         res.send("Missing shopid id.");
@@ -69,15 +84,68 @@ app.get('/api/v1/product/getProducts' , (req,res) =>{
 })
 
 
+app.post('/api/v1/product/statusChanged', (req, res) => {
+    var shopAttrs = {
+        productIDs : req.body.productids,
+        isVerified : req.body.isverified,
+    }
+    product.changeStatusProduct((i) =>{
+            res.send(i);
+    }, shopAttrs);
+});
+
+app.post('/api/v1/product/liveStatusChanged', (req, res) => {
+    var shopAttrs = {
+        productIDs : req.body.productids,
+        isVerified : req.body.isverified,
+    }
+    product.changeLiveStatusProduct((i) =>{
+            res.send(i);
+    }, shopAttrs);
+});
+
 ////////// USER API
 
 app.post('/api/v1/user/validate', (req, res) =>{
     var userDetails = {
         mobile : req.body.mobile,
-        password : req.body.password
+        password : req.body.password,
+        app : req.body.app
     }
-    user.authenticate((i) =>{
-        res.send(i);
+    user.authenticate((i) => {
+        console.log(i)
+        if(i != 'ERROR') {
+            let length = i.length;
+            console.log("length" + i.length);
+            if(length == 1) {
+                    let userIDResp = i[0]['USER_ID'];
+                    console.log(userIDResp);
+                    if(userDetails.app == 'SELLER') {
+                        shopDetail.getShopDetails((ii) =>{
+                                let responseConsturct = {};
+                                responseConsturct['userDetails'] = i;
+                                responseConsturct['shopDetail'] = ii;
+                                res.status(200);
+                                res.send(responseConsturct);
+                        }, userIDResp)
+
+                    }
+                    else {
+                        res.status(200);
+                        res.send(i);
+                    }
+
+            }
+            else {
+                res.status(201);
+                res.send("UnAuthenticate");
+            }
+
+        }
+        else {
+            res.status(201);
+            res.send("UnAuthenticate");
+        }
     }, userDetails);
 });
 
@@ -92,6 +160,27 @@ app.post('/api/v1/product/add', (req, res) =>{
         res.send(i);
     }, userDetails);
 });
+
+app.get('/api/v1/getAllCatogries', (req, res) =>{
+    categories.getBroaderCategory((i) =>{
+        res.send(i);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.post('/upload' , (req, res) =>{
     console.log(req.files)
