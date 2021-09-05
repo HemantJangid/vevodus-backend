ab57cfae32e7bde0d17125e5f4506032398affe4
umesh95 <jangidumesh95@gmail.com>
2021-09-05 18:32:45 +0530

    Add files via upload

--- util/Category.js
+++ util/Category.js
@@ -1,7 +1,7 @@
 const DBUtil = require('./DBUtil');
 
 exports.getBroaderCategory = (callback) =>{
-	var query = "select distinct CATEGORY_NAME from VD_CATEGORY";
+	var query = "select * from VD_CATEGORY";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
 				callback(recordsets['recordset']);
@@ -13,4 +13,34 @@ exports.getBroaderCategory = (callback) =>{
 
 	});
 
+};
+
+
+exports.getAllBrands = (callback) =>{
+	var query = "select * from VD_BRAND";
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback(recordsets['recordset']);
+		}
+		else{
+			callback("Internal Server Error");
+			console.log("Internl server error" + err);
+		}
+
+	});
+
+};
+
+exports.createNewBrand = (callback) => {
+	var query = "select * from VD_BRAND";
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback(recordsets['recordset']);
+		}
+		else{
+			callback("Internal Server Error");
+			console.log("Internl server error" + err);
+		}
+
+	});
 };
