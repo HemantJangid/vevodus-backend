f044828c3eba8c3385df6d39e24e4a9a40dd7e46
umesh95 <jangidumesh95@gmail.com>
2021-09-06 14:44:27 +0530

    Add files via upload

---
+++ util/SyncDBCall.js
@@ -0,0 +1,38 @@
+const sql = require('mssql');
+//const dbConfig = require('./dbconfig');
+let connPoolPromise = null;
+const dbConfig = {
+    user: 'admin',
+    password: 'Password',
+    server: 'vevodus1.cvpftxp1yk7h.ap-south-1.rds.amazonaws.com',
+    port: 1433,
+    database: 'vevodus',
+
+
+    options: {
+    encrypt: false,
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+
+const getConnPoolPromise = () => {
+  if (connPoolPromise) return connPoolPromise;
+
+  connPoolPromise = new Promise((resolve, reject) => {
+    const conn = new sql.ConnectionPool(dbConfig);
+
+    conn.on('close', () => {
+      connPoolPromise = null;
+    });
+
+    conn.connect().then(connPool => {
+      return resolve(connPool);
+    }).catch(err => {
+      connPoolPromise = null;
+      return reject(err);
+    });
+  });
+
+  return connPoolPromise;
+}
+
