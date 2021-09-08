64d11a6f5e1c89d62ad6ce38d902cf62f45b2de9
umesh95 <jangidumesh95@gmail.com>
2021-09-09 01:25:16 +0530

    Add files via upload

--- util/Location.js
+++ util/Location.js
@@ -4,3 +4,16 @@ exports.getLocationID = (locationID) =>{
 	var query =  'select LOCATION_ID from VD_LOCATION where PINCODE = ' + locationID;
 	return query;
 }
+
+exports.getAllPinCodes = (callback) =>{
+	var query =  'select PINCODE as pincode from VD_LOCATION';
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback(recordsets['recordset']);
+		}
+		else{
+			callback("Internal Server Error");
+			console.log("Internl server error" + err);
+		}
+	});
+}
