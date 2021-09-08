46a73f6f729811d5f29580a3e8dd4bbf8ec5d5e7
umesh95 <jangidumesh95@gmail.com>
2021-09-09 01:26:25 +0530

    Add files via upload

--- util/User.js
+++ util/User.js
@@ -2,7 +2,7 @@ const DBUtil = require('./DBUtil');
 
 
 exports.authenticate = (callback, userDetails) =>{
-	var query = "select a.USER_ID, a.Name, a.EMAILADRESS, a.MOBILE, a.QOUTA from VD_USER_PROFILE as a inner join USER_PASSWORD as b on a.USER_ID = b.USER_ID and b.PASSWORD = '" + userDetails.password + "' COLLATE Latin1_General_CS_AS and a.MOBILE='" + userDetails.mobile + "'";
+	var query = "select a.USER_ID as userId, a.Name as name, a.EMAILADRESS as emailAddress, a.MOBILE as mobile, a.QOUTA as quota from VD_USER_PROFILE as a inner join USER_PASSWORD as b on a.USER_ID = b.USER_ID and b.PASSWORD = '" + userDetails.password + "' COLLATE Latin1_General_CS_AS and a.MOBILE='" + userDetails.mobile + "'";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
 				callback(recordsets['recordset']);
