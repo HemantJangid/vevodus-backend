e16009ad119fc8d0577bc22c6f2a4722221de50d
umesh95 <jangidumesh95@gmail.com>
2021-09-08 18:05:22 +0530

    Add files via upload

--- util/Product.js
+++ util/Product.js
@@ -23,6 +23,12 @@ exports.addProducts = (callback, productAttrs) =>{
 
 };
 
+exports.addProductsV2 = (productAttrs) =>{
+	var query = "insert into VD_PRODUCT(NAME, MRP, SP, CATEGORY_ID, Quantity, BRAND_ID, PRODUCT_SPECIFICATION, RETURN_POLICY, VERIFIED, IS_LIVE) "
+" values ('" + productAttrs.productName +"', " + productAttrs.MRP + "," + productAttrs.SP + ", " + productAttrs.categoryID + ", " + productAttrs.quantity + ", " + productAttrs.brandID + ",'" + productAttrs.productSpecification + "'," + productAttrs.returnPolicy + "," + productAttrs.verified + "," + productAttrs.islive + ");SELECT SCOPE_IDENTITY() as primaryKey;";
+	return query;
+}
+
 
 exports.getProducts = (callback, shopID) =>{
 	var query = "select * from VD_PRODUCT where PRODUCT_ID in (SELECT PRODUCT_ID FROM VD_PRODUCT_DETAILS where shop_id = " + shopID + ")";
@@ -122,6 +128,11 @@ exports.addProductMappedToShop = (callback, productAttrs) =>{
 	});
 };
 
+exports.addProductMappedToShopV2 = (productAttrs) =>{
+	var query = "INSERT INTO VD_PRODUCT_DETAILS (PRODUCT_ID, BRAND_ID, SHOP_ID) VALUES (" + productAttrs.productid +"," + productAttrs.brandID +"," + productAttrs.shopID +");";
+	return query;
+};
+
 exports.addProductSpecifications = (callback, productID, key, value) =>{
 	var query = "INSERT INTO VD_PRODCUT_ATTRIBUTES (PRODUCT_ID, ATTR_NAME, ATTR_VAL) VALUES (" + productID +",'"  +  key + "','" +  value + "');";
 	DBUtil.query(query, (err, recordsets) => {
@@ -136,7 +147,12 @@ exports.addProductSpecifications = (callback, productID, key, value) =>{
 	});
 };
 
-exports.addProductPhoto = (callback, productID, photoAdress, isverified) =>{
+exports.addProductSpecificationsV2 = (productID, key, value) =>{
+	var query = "INSERT INTO VD_PRODCUT_ATTRIBUTES (PRODUCT_ID, ATTR_NAME, ATTR_VAL) VALUES (" + productID +",'"  +  key + "','" +  value + "');";
+	return query;
+};
+
+exports.addProductPhoto = (productID, photoAdress, isverified) =>{
 	var query = "INSERT INTO VD_PRODUCT_PHOTO (PRODUCT_ID, PHOTO_ADRESS, VERIFIED) VALUES (" + productID +",'"  +  photoAdress + "'," +  isverified + ");";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
@@ -150,7 +166,8 @@ exports.addProductPhoto = (callback, productID, photoAdress, isverified) =>{
 	});
 };
 
-exports.updateDefaultPhotoLink = (callback, productID, photoLink) =>{
+
+exports.updateDefaultPhotoLink = (productID, photoLink) =>{
 	var query = "UPDATE VD_PRODUCT SET PHOTO_LINK = '"+ photoLink + "' WHERE PRODUCT_ID in (" + productID +")";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
