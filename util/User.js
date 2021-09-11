52f77ed6993e5f0a4067ee18f8de23fc7f98a8d0
umesh95 <jangidumesh95@gmail.com>
2021-09-12 02:41:04 +0530

    Add files via upload

--- util/User.js
+++ util/User.js
@@ -3,6 +3,7 @@ const DBUtil = require('./DBUtil');
 
 exports.authenticate = (callback, userDetails) =>{
 	var query = "select a.USER_ID as userId, a.Name as name, a.EMAILADRESS as emailAddress, a.MOBILE as mobile, a.QOUTA as quota from VD_USER_PROFILE as a inner join USER_PASSWORD as b on a.USER_ID = b.USER_ID and b.PASSWORD = '" + userDetails.password + "' COLLATE Latin1_General_CS_AS and a.MOBILE='" + userDetails.mobile + "'";
+	console.log(query);
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
 				callback(recordsets['recordset']);
@@ -32,3 +33,58 @@ exports.addUserRoleMapped = (userAttrs) => {
 	query += " values (" + userAttrs['userID'] +",'" + userAttrs['role']+ "');";
 	return query;
 }
+exports.addNewUserPassword = (userAttrs) => {
+	var query = "insert into USER_PASSWORD(USER_ID, PASSWORD) " 
+	query += " values (" + userAttrs['userID'] +",'" + userAttrs['password']+ "');";
+	return query;
+}
+
+exports.addUserPhoto = (callback, productID, photoAdress, isverified) =>{
+	var query = "INSERT INTO USER_PHOTO (USER_ID, PHOTO_ADRESS, VERIFIED) VALUES (" + productID +",'"  +  photoAdress + "'," +  isverified + ");";
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback("OK");
+		}
+		else{
+			callback("Internal Server Error" + err);
+			console.log("Internl in shop image  error" + err);
+		}
+
+	});
+};
+
+exports.addCheckout = (callback, userID, productID) =>{
+	var query = "INSERT INTO VD_USER_CHECKOUT (USER_ID, PRODUCT_ID, STATUS) VALUES (" + userID +","  +  productID + ",'N');";
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback("OK");
+		}
+		else{
+			callback("Internal Server Error" + err);
+			console.log("Internl in checkout image  error" + err);
+		}
+
+	});
+};
+
+
+exports.changeStatusCheckout = (callback, checkoutID, status) =>{
+	var query = "UPDATE VD_USER_CHECKOUT SET status = '"+ status + "' WHERE CHECKOUT_ID in (" + checkoutID +")";
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback("OK");
+		}
+		else{
+			callback("Internal Server Error");
+			console.log("Internl server error in updating status for checkout" + err);
+		}
+	});
+}
+
+exports.getUserInfo = (userId) => {
+	var query = "select USER_ID as userId, NAME as name, mobile as mobile, EMAILADRESS as emailAddress, QOUTA as qouta from VD_USER_PROFILE where USER_ID=" +userId;
+	return query;
+}
+
+
+
