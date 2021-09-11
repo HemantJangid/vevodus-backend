942ce89104b705da409f7908ce14b135a7cebba5
umesh95 <jangidumesh95@gmail.com>
2021-09-12 02:40:49 +0530

    Add files via upload

--- util/ShopController.js
+++ util/ShopController.js
@@ -2,7 +2,7 @@ const DBUtil = require('./DBUtil');
 
 exports.getShopDetails = (callback, userID) =>{
 	console.log(userID + "checking");
-	var query =  'select shop.SHOP_ID as shopId, shop.NAME as shopName, shop.LAT as lat, shop.lang as lang, shop.ADRESS as address, shop.LOCATION_ID as locationId, shop.BROADER_CATEGORY as broaderCategory, shop.GST as gst, shop.VERIFIED as verified, shop.PHOTO_LINK as photoLink from VD_SHOP as shop inner join VD_SHOP_OWNER as sowner on  sowner.USER_ID =' + userID;
+	var query =  'select shop.SHOP_ID as shopId, shop.NAME as shopName, shop.LAT as lat, shop.lang as lang, shop.ADRESS as address,  shop.BROADER_CATEGORY as broaderCategory, shop.GST as gst, shop.VERIFIED as verified, shop.PHOTO_LINK as photoLink from VD_SHOP as shop inner join VD_SHOP_OWNER as sowner on  sowner.USER_ID =' + userID;
 	DBUtil.query(query, (err, recordsets) => {
 	if(err == null) {
 			callback(recordsets['recordset']);
@@ -17,7 +17,7 @@ exports.getShopDetails = (callback, userID) =>{
 
 
 exports.getAllShops = (callback) => {
-	var query = " select  NAME as name, LAT as lat, LANG as lang, ADRESS as address, LOCATION_ID as locationId, BROADER_CATEGORY as broaderCategory, GST as gst, VERIFIED as verified, PHOTO_LINK as photoLink from VD_SHOP";
+	var query = " select  NAME as name, LAT as lat, LANG as lang, ADRESS as address,  BROADER_CATEGORY as broaderCategory, GST as gst, VERIFIED as verified, PHOTO_LINK as photoLink from VD_SHOP";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
 				callback(recordsets['recordset']);
@@ -58,8 +58,8 @@ exports.getPhotosLink = (callback, productid) =>{
 };
 
 exports.addNewShop = (shopAttrs) =>{
-	var query = "insert into VD_SHOP(NAME, LAT, LANG, ADRESS, LOCATION_ID, BROADER_CATEGORY, GST, VERIFIED)"
-	query += " values('" + shopAttrs['shopName'] + "', '" + shopAttrs['lat'] +"','" + shopAttrs['lang'] +"', '" + shopAttrs['address'] +"'" + shopAttrs['locationID'] +",'" + shopAttrs['broaderCategory'] + "','" + shopAttrs['GST']+"',0); SELECT SCOPE_IDENTITY() as shopid;";
+	var query = "insert into VD_SHOP(NAME, LAT, LANG, ADRESS, BROADER_CATEGORY, GST, VERIFIED)"
+	query += " values('" + shopAttrs['shopName'] + "', '" + shopAttrs['lat'] +"','" + shopAttrs['lang'] +"', '" + shopAttrs['address'] +"','" + shopAttrs['broaderCategory'] + "','" + shopAttrs['GST']+"',0); SELECT SCOPE_IDENTITY() as shopid;";
 	return query;
 }
 
@@ -69,7 +69,7 @@ exports.addUserShopMapped = (userAttrs) => {
 	return query;
 }
 
-exports.addShopPhoto = (productID, photoAdress, isverified) =>{
+exports.addShopPhoto = (callback, productID, photoAdress, isverified) =>{
 	var query = "INSERT INTO SHOP_PHOTO (SHOP_ID, PHOTO_ADRESS, VERIFIED) VALUES (" + productID +",'"  +  photoAdress + "'," +  isverified + ");";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
@@ -83,7 +83,7 @@ exports.addShopPhoto = (productID, photoAdress, isverified) =>{
 	});
 };
 
-exports.updateDefaultPhotoLink = (shopID, photoLink) =>{
+exports.updateDefaultPhotoLink = (callback, shopID, photoLink) =>{
 	var query = "UPDATE VD_SHOP SET PHOTO_LINK = '"+ photoLink + "' WHERE SHOP_ID in (" + shopID +")";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
@@ -92,6 +92,20 @@ exports.updateDefaultPhotoLink = (shopID, photoLink) =>{
 		else{
 			callback("Internal Server Error");
 			console.log("Adding default link .Internl server error" + err);
+		}
+	});
+}
+
+
+exports.changeStatusCheckout = (callback, checkoutID, status) =>{
+	var query = "UPDATE VD_USER_CHECKOUT SET status = '"+ status + "' WHERE CHECKOUT_ID in (" + checkoutID +")";
+	DBUtil.query(query, (err, recordsets) => {
+		if(err == null) {
+				callback("OK");
+		}
+		else{
+			callback("Internal Server Error");
+			console.log("Internl server error in updating status for checkout" + err);
 		}
 	});
 }
