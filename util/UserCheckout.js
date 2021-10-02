e03eef7e9ca76b120d5553b97cc28468ec1aec90
umesh95 <jangidumesh95@gmail.com>
2021-10-02 12:00:49 +0530

    Add files via upload

--- util/UserCheckout.js
+++ util/UserCheckout.js
@@ -1,7 +1,7 @@
 const DBUtil = require('./DBUtil');
 
 exports.getActiveUserCheckoutDetails = (callback, checkoutAttrs) =>{
-	var query = "select p.NAME as name, p.MRP as mrp, p.SP as sp, p.CATEGORY_ID as categoryId, p.PRODUCT_SPECIFICATION as productSpecification ,p.PHOTO_LINK as photoLink ,CHECKOUT_ID as checkoutID, USER_ID as userID, p.PRODUCT_ID as productId, STATUS as status, ud.Quantity as quantity, Date_ORDERED as dateOrdered  from VD_PRODUCT as p inner join VD_USER_CHECKOUT as ud on p.PRODUCT_ID = ud.PRODUCT_ID and ud.USER_ID = " +checkoutAttrs + " and (ud.status = 'A' or ud.status = 'N') order by ud.Date_ORDERED;";
+	var query = "select p.NAME as name, p.MRP as mrp, p.SP as sp, p.CATEGORY_ID as categoryId, p.PRODUCT_SPECIFICATION as productSpecification ,p.PHOTO_LINK as photoLink ,CHECKOUT_ID as checkoutID, USER_ID as userId, p.PRODUCT_ID as productId, STATUS as status, ud.Quantity as quantity, Date_ORDERED as dateOrdered  from VD_PRODUCT as p inner join VD_USER_CHECKOUT as ud on p.PRODUCT_ID = ud.PRODUCT_ID and ud.USER_ID = " +checkoutAttrs + " and (ud.status = 'A' or ud.status = 'N') order by ud.Date_ORDERED;";
 	//var query =  "select CHECKOUT_ID as checkoutID, USER_ID as userID, PRODUCT_ID as productId, STATUS as status, Quantity as quantity, Date_ORDERED as dateOrdered from VD_USER_CHECKOUT where USER_ID = " + checkoutAttrs + "and (status = 'A' or status = 'N') order by Date_ORDERED";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
@@ -15,7 +15,7 @@ exports.getActiveUserCheckoutDetails = (callback, checkoutAttrs) =>{
 }
 
 exports.getPastUserCheckoutDetails = (callback, checkoutAttrs) =>{
-	var query = "select p.NAME as name, p.MRP as mrp, p.SP as sp, p.CATEGORY_ID as categoryId, p.PRODUCT_SPECIFICATION as productSpecification ,p.PHOTO_LINK as photoLink ,CHECKOUT_ID as checkoutID, USER_ID as userID, p.PRODUCT_ID as productId, STATUS as status, ud.Quantity as quantity, Date_ORDERED as dateOrdered  from VD_PRODUCT as p inner join VD_USER_CHECKOUT as ud on p.PRODUCT_ID = ud.PRODUCT_ID and ud.USER_ID = " +checkoutAttrs + " and (ud.status = 'D' or ud.status = 'X' or ud.status = 'C') order by ud.Date_ORDERED;";
+	var query = "select p.NAME as name, p.MRP as mrp, p.SP as sp, p.CATEGORY_ID as categoryId, p.PRODUCT_SPECIFICATION as productSpecification ,p.PHOTO_LINK as photoLink ,CHECKOUT_ID as checkoutID, USER_ID as userId, p.PRODUCT_ID as productId, STATUS as status, ud.Quantity as quantity, Date_ORDERED as dateOrdered  from VD_PRODUCT as p inner join VD_USER_CHECKOUT as ud on p.PRODUCT_ID = ud.PRODUCT_ID and ud.USER_ID = " +checkoutAttrs + " and (ud.status = 'D' or ud.status = 'X' or ud.status = 'C') order by ud.Date_ORDERED;";
 	//var query =  "select CHECKOUT_ID as checkoutID, USER_ID as userID, PRODUCT_ID as productId, STATUS as status, Quantity as quantity, Date_ORDERED as dateOrdered from VD_USER_CHECKOUT where USER_ID = " + checkoutAttrs + "and (status = 'D' or status = 'X' or status = 'C') order by Date_ORDERED";
 	DBUtil.query(query, (err, recordsets) => {
 		if(err == null) {
